3f0a7cefcb41b44e103e29d1cfe8375430f287f0
Crucialjun <nich.otieno@gmail.com>
2022-12-05 23:59:52 +0300

    try out composables

--- app/src/main/java/com/example/composerecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/composerecipeapp/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.composerecipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composerecipeapp.ui.theme.ComposeRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text(text = "Hey look some text")
+                        Spacer(modifier=Modifier.padding(top = 10.dp))
+                        Button(onClick = { /*TODO*/ }) {
+                            Text("A Button")
+                        }
+                    }
                 }
             }
         }
