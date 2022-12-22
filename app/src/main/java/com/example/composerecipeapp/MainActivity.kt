f56839a39c53578dfb0abe8fae2e420c8b093430
Crucialjun <nich.otieno@gmail.com>
2022-12-22 23:59:13 +0300

    add assets

--- app/src/main/java/com/example/composerecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/composerecipeapp/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.composerecipeapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composerecipeapp.ui.theme.ComposeRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,15 +39,50 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFFF2F2F2)
                 ) {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Text(text = "Hey look some text")
-                        Spacer(modifier=Modifier.padding(top = 10.dp))
-                        Button(onClick = { /*TODO*/ }) {
-                            Text("A Button")
+                    Column(
+                        Modifier.verticalScroll(rememberScrollState())
+                    ) {
+                        Row() {
+
+                        }
+                        Image(
+                            painter = painterResource(id = R.drawable.happy_meal_small),
+                            contentDescription = "Happy Meal Image",
+                            Modifier.height(300.dp),
+                            contentScale = ContentScale.Crop
+                        )
+                        Column(
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(
+                                "Happy Meal",
+                                style = TextStyle(
+                                    fontSize = 26.sp
+                                )
+                            )
+                            Spacer(Modifier.padding(top = 10.dp))
+                            Text(
+                                "800 Calories", style = TextStyle(
+                                    fontSize = 17.sp
+                                )
+                            )
+                            Spacer(Modifier.padding(top = 10.dp))
+                            Text(
+                                "$5.99",
+                                style = TextStyle(
+                                    fontSize = 17.sp,
+                                    color = Color(0xFF85bb65)
+
+                                )
+                            )
+
+
                         }
                     }
+
+
                 }
             }
         }
